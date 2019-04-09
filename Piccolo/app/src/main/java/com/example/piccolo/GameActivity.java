d162772c6987875c585ed266b801c1205889337d
Mirco Cafaro <mirco.cafaro@sap.com>
2019-04-09 16:16:03 +0200

    Final state of picoolo sap app

--- Piccolo/app/src/main/java/com/example/piccolo/GameActivity.java
+++ Piccolo/app/src/main/java/com/example/piccolo/GameActivity.java
@@ -3,21 +3,35 @@ package com.example.piccolo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     private ArrayList<String> players;
     private ArrayList<String> questionAll;
-    private ArrayList<String> questionPerson;
+    private ArrayList<String> questionPlayer;
+    private TextView textViewQuestion;
+    private Button buttonNext;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        this.loadData();
+        this.textViewQuestion = findViewById(R.id.viewGameTask);
+        this.buttonNext = findViewById(R.id.nextTextButton);
         this.questionAll = new ArrayList<>();
-        this.questionPerson = new ArrayList<>();
+        this.questionPlayer = new ArrayList<>();
+        this.counter = 0;
+        this.loadData();
+        this.loadQuestionPerson();
+        this.loadQuestionsAll();
+        this.textViewQuestion.setText(this.getRandomQuestion());
     }
 
     private void loadData() {
@@ -25,6 +39,22 @@ public class GameActivity extends AppCompatActivity {
         this.players = intent.getStringArrayListExtra(MainActivity.PLAYERS);
     }
 
+    private String getRandomQuestion() {
+        this.counter ++;
+        Random rand = new Random();
+        if (rand.nextBoolean()) {
+            //questionall
+            int randomQuestionNumber = rand.nextInt(this.questionAll.size());
+            return this.questionAll.get(randomQuestionNumber);
+        } else {
+            //question player
+            int randomQuestionNumber = rand.nextInt(this.questionPlayer.size());
+            String randomQuestion = this.questionPlayer.get(randomQuestionNumber);
+            int randomPlayerNumber = rand.nextInt(this.players.size());
+            String randomPlayer = this.players.get(randomPlayerNumber);
+            return randomPlayer + randomQuestion;
+        }
+    }
     private void loadQuestionsAll () {
         this.questionAll.add("Warum braucht man überhautp einen Grund zum Trinken? Trinkt alle 3 Schlücke!");
         this.questionAll.add("Lieber die Simpsons, oder Family Guy? Stimmt alle gleichzeitig ab, die Minderheit muss 4 Schlücke trinken.");
@@ -91,57 +121,68 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void loadQuestionPerson () {
-        this.questionPerson.add(" verteil so viele Schlücke, wie du schon ausgetrunkene Gläser an diesem Abend hast.");
-        this.questionPerson.add(" mache jemanden ein Kompliment über sein Bein, falls es schlecht ist, trinke 3 Schlücke, ansonsten verteile sie.");
-        this.questionPerson.add(" hast du schon einmal Lippenstift verwendet, falls ja dann trinke 2 Schlücke, ansonsten verteil sie.");
-        this.questionPerson.add(" macht drei Kniebeugen (aber gute Kniebeugen) und trinkt nach jedem zwei Schlücke!");
-        this.questionPerson.add(" zähl alle Vornamen und das Gewicht jeder einzelnen Person am Tisch auf, wenn du abloost, dann trinke 4 Schlücke.");
-        this.questionPerson.add(" wenn du schon einmal auf dem Klo überrascht wurdest (bei was auch immer), dann trinke 3 Schlücke, ansonsten verteil sie.");
-        this.questionPerson.add(" wenn du ein Bart hast, dann trinke 2 Schlücke. (Falls du ein Mädchen bist, dann darfst du das dreifache an Schlücke verteilen, wenn es stimmt).");
-        this.questionPerson.add(" bist du schon einmal zu spät zum Unterricht/Vorlesung gekommen, dann trinke 3 Schlücke als Bestrafung. Aber #Rebell an dieser Stelle!");
-        this.questionPerson.add(" erklärt kurz die Handlung eines Films, ohne dabei Namen oder Filmtitel zu sagen. Die erste Person die es errät, verteilt 4 Schlücke.");
-        this.questionPerson.add(" hast du schon einmal schwarz gearbeitet, dann trinke bitte 3 Schlücke und warte bis der Zoll kommt! Danke für deine Kooperation.");
-        this.questionPerson.add(", wenn du noch nie einen Harry Potter Film gesehen hast, dann schlag dir kurz selber auf den Kopf und trinke 3 Schlücke.");
-        this.questionPerson.add(" trinkt jetzt einfach mal 2 Schlücke. #Opfer");
-        this.questionPerson.add(" trinkt so viel Schlücke, wie er heute telefoniert hat.");
-        this.questionPerson.add(" trinkt 5 Schlücke, dein rechter nachbar einen Schluck weniger und so weiter, bis ihr bei Null angekommen seid.");
-        this.questionPerson.add(" trinkt 2 Schlücke und sagt dabei Lautstark 'Bazinga ihr Leleks' - mit vollem Mund.");
-        this.questionPerson.add(" errate die Farbe der Unterhose deines rechten Nachbar. Wenn es richtig ist verteil 4 Schlücke. Wenn nicht, dann trinke sie selbst.");
-        this.questionPerson.add(" wenn du weniger als 70 kg wiegst, dann trinke 3 Schlücke und freue dich über die paar kcal mehr.");
-        this.questionPerson.add(" hast du schon einmal geschlafen, dann trinken alle anderen 5 Schlücke. #senseless");
-        this.questionPerson.add(" versucht das Alphabet rückwärts aufzusagen, jeder Fehler ist ein Schluck für ihn/sie!");
-        this.questionPerson.add(" trinke 5 Schlücke, wenn du beim Toilettengang zu Hause die Tür offen lässt. Du Ekelbob!");
-        this.questionPerson.add(" ist etwas arroganter als der Durchschnitt? Dann trinkt er/sie 4 Schlücke, ansonsten verteile sie. #Alphakevin");
-        this.questionPerson.add(" wenn du in den letzten vier Wochen ein Buch gelesen hast, dann verteile 7 Schlücke, ansonsten steh auf, geh zur nächsten Bücherei und kauf es dir und währenddessen trinke 3 Schlücke.");
-        this.questionPerson.add(" sag entweder auswendig die Adresse oder eine Telefonnummer deines linken Nachbarn auf, oder trinke 2 Schlücke.");
-        this.questionPerson.add(" zieh ein Kleidungsstück aus, oder trinke 5 Schlücke. #sexymoment");
-        this.questionPerson.add(" erzählt eine peinliche Geschichte über einen anderen Mitspieler am Tisch, oder trinkt 3 Schlücke.");
-        this.questionPerson.add(" schließe die Augen und sag die Augenfarben der Mitspieler am Tisch auf, wenn du fehlschlägst (peinlich), trinke 2 Schlücke.");
-        this.questionPerson.add(" beginnt. Im Kreis, zählt Schauspieler aus den Marvelfilmen auf. Wer nichts mehr weiß trinkt 3 Schlücke.");
-        this.questionPerson.add(" versuche auf einem Bein zu stehen, deine Nase zu berühren und dabei deine Augen im Wechseln zu schließen. Bei einem Fail gibt es 3 Schlücke für dich.");
-        this.questionPerson.add(" hast du schon einmal ein Tanga angehabt? Falls ja, dann trinke 3 Schlücke. Falls du auch noch ein Junge sein solltest, dann trinke das Doppelte! #Strapsenjunge");
-        this.questionPerson.add(" hat jetzt Zahnweh und wie jeder gute Zahnarzt trinkt er erstmal 4 Schlücke gegen die Schmerzen.");
-        this.questionPerson.add(" riecht nach Fisch aus dem Mund. #Ekelhaft. Trinke 3 Schlücke und iss dann eine Zitronenscheibe!");
-        this.questionPerson.add(", wenn du einen Führerschein hast und schon einaml den Motor gestartet hast, ohne dabei die Kupplung gedrückt zu haben, dann trinke 3 Schlücke auf dein Abkacken!");
-        this.questionPerson.add(" sein rechter Nachbar trinkt 3 Schlücke, währenddessen diese(r) komplimente mit vollem Mund ausspricht! (Nicht sabbern!)");
-        this.questionPerson.add(" summe ein Lied deiner Wahl. Wer es errät darf 8 Schlücke verteilen.");
-        this.questionPerson.add(" schenkt eine Runde shotz aus! Prost");
-        this.questionPerson.add(", wenn du rauchst, dann wirf eine Zigarette in den Mülleimer und nimm 4 Schlücke. Falls nicht, dann freut sich deine Lunge, aber deine Leber bekommt 2 Schlücke ab.");
-        this.questionPerson.add(", entscheide wer von euch am sensibelsten ist. Diese Person trinkt 2 Schlücke und versucht mal nicht zu heulen.");
-        this.questionPerson.add(", verteil 5 Schlücke an die Person, mit der du nur peinliches erlebt hast!");
-        this.questionPerson.add(" hast du schon einmal während des Unterrichts dein Handy für außerunterrichtliche Zwecke benutzt? Wer nicht!? Alle anderen trinken 4 Schlücke.");
-        this.questionPerson.add(", denkst du es wäre ok, wenn du jetzt 5 Schlücke trinken müsstest? Da diese App keine Gefühle kennt, tu es einfach!");
-        this.questionPerson.add(", wenn du schon einmal außerhalb der Schule ein Gedicht geschrieben hast, dann trinke 3 Schlücke! #sweetie");
-        this.questionPerson.add(", alle mit denen du bei Snapchat befreundet bist, trinken 3 Schlücke.");
-        this.questionPerson.add(" ist jetzt einfach mal die schönste Person unter euch. Trinkt alle 2 Schlücke darauf!");
-        this.questionPerson.add(" würdest du lieber 15 Schlücke trinken, oder alle 4 Schlücke trinken lassen? Wähle deine Antwort!");
-        this.questionPerson.add(", wenn du älter als 20 bist, dann trinke 3 Schlücke, ansonsten verteile sie!");
-        this.questionPerson.add(", zähl auf wen du magst. Alle die damit einverstanden sind trinken einen Schluck.");
-        this.questionPerson.add(" führe 5 Sekunden einen Stepptanz auf oder trinke 3 Schlücke.");
-        this.questionPerson.add(", wenn du drei Songs von Max Giesinger nennen kannst, dann verteile 3 Schlücke, ansonsten trinke sie!");
-        this.questionPerson.add(", wenn du dir schon einaml auf den Hinter geschlagen hast und dabei gesagt hast 'Hit me baby one more Time', dann verteile 12 Schlücke, ansonsten trinke 3 Schlücke.");
-        this.questionPerson.add(", wenn dein Bartwuchs sehr dünn verteilt ist (also so Fleckenhaft halt), dann trinke 3 Schlücke!");
-        this.questionPerson.add(" ist jetzt der König der Löwen. Versucht ihn hochzuhalten und dann die Szene aus König der Löwen nachzuspielen. Ihr müsst nichts trinken, ist auch so Lustig.");
+        this.questionPlayer.add(" verteil so viele Schlücke, wie du schon ausgetrunkene Gläser an diesem Abend hast.");
+        this.questionPlayer.add(" mache jemanden ein Kompliment über sein Bein, falls es schlecht ist, trinke 3 Schlücke, ansonsten verteile sie.");
+        this.questionPlayer.add(" hast du schon einmal Lippenstift verwendet, falls ja dann trinke 2 Schlücke, ansonsten verteil sie.");
+        this.questionPlayer.add(" macht drei Kniebeugen (aber gute Kniebeugen) und trinkt nach jedem zwei Schlücke!");
+        this.questionPlayer.add(" zähl alle Vornamen und das Gewicht jeder einzelnen Person am Tisch auf, wenn du abloost, dann trinke 4 Schlücke.");
+        this.questionPlayer.add(" wenn du schon einmal auf dem Klo überrascht wurdest (bei was auch immer), dann trinke 3 Schlücke, ansonsten verteil sie.");
+        this.questionPlayer.add(" wenn du ein Bart hast, dann trinke 2 Schlücke. (Falls du ein Mädchen bist, dann darfst du das dreifache an Schlücke verteilen, wenn es stimmt).");
+        this.questionPlayer.add(" bist du schon einmal zu spät zum Unterricht/Vorlesung gekommen, dann trinke 3 Schlücke als Bestrafung. Aber #Rebell an dieser Stelle!");
+        this.questionPlayer.add(" erklärt kurz die Handlung eines Films, ohne dabei Namen oder Filmtitel zu sagen. Die erste Person die es errät, verteilt 4 Schlücke.");
+        this.questionPlayer.add(" hast du schon einmal schwarz gearbeitet, dann trinke bitte 3 Schlücke und warte bis der Zoll kommt! Danke für deine Kooperation.");
+        this.questionPlayer.add(", wenn du noch nie einen Harry Potter Film gesehen hast, dann schlag dir kurz selber auf den Kopf und trinke 3 Schlücke.");
+        this.questionPlayer.add(" trinkt jetzt einfach mal 2 Schlücke. #Opfer");
+        this.questionPlayer.add(" trinkt so viel Schlücke, wie er heute telefoniert hat.");
+        this.questionPlayer.add(" trinkt 5 Schlücke, dein rechter nachbar einen Schluck weniger und so weiter, bis ihr bei Null angekommen seid.");
+        this.questionPlayer.add(" trinkt 2 Schlücke und sagt dabei Lautstark 'Bazinga ihr Leleks' - mit vollem Mund.");
+        this.questionPlayer.add(" errate die Farbe der Unterhose deines rechten Nachbar. Wenn es richtig ist verteil 4 Schlücke. Wenn nicht, dann trinke sie selbst.");
+        this.questionPlayer.add(" wenn du weniger als 70 kg wiegst, dann trinke 3 Schlücke und freue dich über die paar kcal mehr.");
+        this.questionPlayer.add(" hast du schon einmal geschlafen, dann trinken alle anderen 5 Schlücke. #senseless");
+        this.questionPlayer.add(" versucht das Alphabet rückwärts aufzusagen, jeder Fehler ist ein Schluck für ihn/sie!");
+        this.questionPlayer.add(" trinke 5 Schlücke, wenn du beim Toilettengang zu Hause die Tür offen lässt. Du Ekelbob!");
+        this.questionPlayer.add(" ist etwas arroganter als der Durchschnitt? Dann trinkt er/sie 4 Schlücke, ansonsten verteile sie. #Alphakevin");
+        this.questionPlayer.add(" wenn du in den letzten vier Wochen ein Buch gelesen hast, dann verteile 7 Schlücke, ansonsten steh auf, geh zur nächsten Bücherei und kauf es dir und währenddessen trinke 3 Schlücke.");
+        this.questionPlayer.add(" sag entweder auswendig die Adresse oder eine Telefonnummer deines linken Nachbarn auf, oder trinke 2 Schlücke.");
+        this.questionPlayer.add(" zieh ein Kleidungsstück aus, oder trinke 5 Schlücke. #sexymoment");
+        this.questionPlayer.add(" erzählt eine peinliche Geschichte über einen anderen Mitspieler am Tisch, oder trinkt 3 Schlücke.");
+        this.questionPlayer.add(" schließe die Augen und sag die Augenfarben der Mitspieler am Tisch auf, wenn du fehlschlägst (peinlich), trinke 2 Schlücke.");
+        this.questionPlayer.add(" beginnt. Im Kreis, zählt Schauspieler aus den Marvelfilmen auf. Wer nichts mehr weiß trinkt 3 Schlücke.");
+        this.questionPlayer.add(" versuche auf einem Bein zu stehen, deine Nase zu berühren und dabei deine Augen im Wechseln zu schließen. Bei einem Fail gibt es 3 Schlücke für dich.");
+        this.questionPlayer.add(" hast du schon einmal ein Tanga angehabt? Falls ja, dann trinke 3 Schlücke. Falls du auch noch ein Junge sein solltest, dann trinke das Doppelte! #Strapsenjunge");
+        this.questionPlayer.add(" hat jetzt Zahnweh und wie jeder gute Zahnarzt trinkt er erstmal 4 Schlücke gegen die Schmerzen.");
+        this.questionPlayer.add(" riecht nach Fisch aus dem Mund. #Ekelhaft. Trinke 3 Schlücke und iss dann eine Zitronenscheibe!");
+        this.questionPlayer.add(", wenn du einen Führerschein hast und schon einaml den Motor gestartet hast, ohne dabei die Kupplung gedrückt zu haben, dann trinke 3 Schlücke auf dein Abkacken!");
+        this.questionPlayer.add(" sein rechter Nachbar trinkt 3 Schlücke, währenddessen diese(r) komplimente mit vollem Mund ausspricht! (Nicht sabbern!)");
+        this.questionPlayer.add(" summe ein Lied deiner Wahl. Wer es errät darf 8 Schlücke verteilen.");
+        this.questionPlayer.add(" schenkt eine Runde shotz aus! Prost");
+        this.questionPlayer.add(", wenn du rauchst, dann wirf eine Zigarette in den Mülleimer und nimm 4 Schlücke. Falls nicht, dann freut sich deine Lunge, aber deine Leber bekommt 2 Schlücke ab.");
+        this.questionPlayer.add(", entscheide wer von euch am sensibelsten ist. Diese Person trinkt 2 Schlücke und versucht mal nicht zu heulen.");
+        this.questionPlayer.add(", verteil 5 Schlücke an die Person, mit der du nur peinliches erlebt hast!");
+        this.questionPlayer.add(" hast du schon einmal während des Unterrichts dein Handy für außerunterrichtliche Zwecke benutzt? Wer nicht!? Alle anderen trinken 4 Schlücke.");
+        this.questionPlayer.add(", denkst du es wäre ok, wenn du jetzt 5 Schlücke trinken müsstest? Da diese App keine Gefühle kennt, tu es einfach!");
+        this.questionPlayer.add(", wenn du schon einmal außerhalb der Schule ein Gedicht geschrieben hast, dann trinke 3 Schlücke! #sweetie");
+        this.questionPlayer.add(", alle mit denen du bei Snapchat befreundet bist, trinken 3 Schlücke.");
+        this.questionPlayer.add(" ist jetzt einfach mal die schönste Person unter euch. Trinkt alle 2 Schlücke darauf!");
+        this.questionPlayer.add(" würdest du lieber 15 Schlücke trinken, oder alle 4 Schlücke trinken lassen? Wähle deine Antwort!");
+        this.questionPlayer.add(", wenn du älter als 20 bist, dann trinke 3 Schlücke, ansonsten verteile sie!");
+        this.questionPlayer.add(", zähl auf wen du magst. Alle die damit einverstanden sind trinken einen Schluck.");
+        this.questionPlayer.add(" führe 5 Sekunden einen Stepptanz auf oder trinke 3 Schlücke.");
+        this.questionPlayer.add(", wenn du drei Songs von Max Giesinger nennen kannst, dann verteile 3 Schlücke, ansonsten trinke sie!");
+        this.questionPlayer.add(", wenn du dir schon einaml auf den Hinter geschlagen hast und dabei gesagt hast 'Hit me baby one more Time', dann verteile 12 Schlücke, ansonsten trinke 3 Schlücke.");
+        this.questionPlayer.add(", wenn dein Bartwuchs sehr dünn verteilt ist (also so Fleckenhaft halt), dann trinke 3 Schlücke!");
+        this.questionPlayer.add(" ist jetzt der König der Löwen. Versucht ihn hochzuhalten und dann die Szene aus König der Löwen nachzuspielen. Ihr müsst nichts trinken, ist auch so Lustig.");
+    }
+
+    public void nextQuestion(View view) {
+        if(this.counter < 20) {
+            this.textViewQuestion.setText(this.getRandomQuestion());
+        } else if (this.counter == 20) {
+            this.textViewQuestion.setText(this.getRandomQuestion());
+            this.buttonNext.setText("END");
+        } else {
+            finish();
+        }
     }
 }
 
